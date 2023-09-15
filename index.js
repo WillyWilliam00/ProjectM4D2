d55380cf0d708eccc60eafb2a19ebdd6b2003272
WillyWilliam00 <costawilliam98@gmail.com>
2023-09-15 21:09:22 +0200

    up

--- index.js
+++ index.js
@@ -1,54 +1,32 @@
-// variabili da gestire:
-// row dispay none
-// div dove agganciare gli album 
-
-
+const SearchArtista = document.querySelector("#searchField")
+function search() {
+    if(SearchArtista.value){
+        FetchTotale(SearchArtista.value)
+    } else {
+        alert("inserisci un'artista!")
+    } 
+}   
 
 function FetchTotale(artista) {
     const IdArtista = document.querySelector(`#${artista}`)
     const Row = document.querySelector(`#${artista}Section`)
     const SearchResults = document.querySelector("#searchResults")
-        SearchResults.removeAttribute('style');
-    
+    SearchResults.removeAttribute('style');
     fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${artista}`)
         .then(response => response.json())
         .then(body => {
             const music = body.data
             const FiveRandomMusic = music.slice(0, 4)
-            console.log(FiveRandomMusic)
             IdArtista.className = "pt-2"
             for (let i = 0; i < FiveRandomMusic.length; i++) {
-               
-                 
-                
                 Row.innerHTML += `<div class="col-3 d-flex flex-column align-items-center">
-                                        <img  src="${FiveRandomMusic[i].album.cover_medium}"><p class="pt-4 text-center">${FiveRandomMusic[i].title}</p>
+                                    <h4>Album:</h4>
+                                    <p style="font-size: 12px;">${FiveRandomMusic[i].album.title}</p>
+                                    <img  src="${FiveRandomMusic[i].album.cover_medium}">
+                                    <h6 class="pt-4 text-center">Titolo:</h6>
+                                    <p class="text-center">${FiveRandomMusic[i].title}</p>
                                     </div>`
-                             
             }   
         })
-        
-        
-
-        
-       
 }
-
-function album() {
- 
-    for (let i = 0; i < FiveRandomMusic.length; i++) {
-           
-        SearchResults.innerHTML += `<div><p>${FiveRandomMusic[i].album.title}</p></div>`  
-        
-        
-                     
-     }
-
-   
-}
-
-    FetchTotale("eminem")
-    FetchTotale("metallica")
-    FetchTotale("queen")
-    
 
