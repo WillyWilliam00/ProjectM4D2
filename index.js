31c9e6bc08a45c41c41f5efa76de899e0ebcfd1a
WillyWilliam00 <133374335+WillyWilliam00@users.noreply.github.com>
2023-09-15 18:25:35 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-const Row = document.querySelector("#eminemSection")
+// variabili da gestire:
+// row dispay none
+// div dove agganciare gli album 
+
+
+
+function FetchTotale(artista) {
+    const IdArtista = document.querySelector(`#${artista}`)
+    const Row = document.querySelector(`#${artista}Section`)
+    const SearchResults = document.querySelector("#searchResults")
+        SearchResults.removeAttribute('style');
+    
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${artista}`)
+        .then(response => response.json())
+        .then(body => {
+            const music = body.data
+            const FiveRandomMusic = music.slice(0, 4)
+            console.log(FiveRandomMusic)
+            IdArtista.className = "pt-2"
+            for (let i = 0; i < FiveRandomMusic.length; i++) {
+               
+                 
+                
+                Row.innerHTML += `<div class="col-3 d-flex flex-column align-items-center">
+                                        <img  src="${FiveRandomMusic[i].album.cover_medium}"><p class="pt-4 text-center">${FiveRandomMusic[i].title}</p>
+                                    </div>`
+                             
+            }   
+        })
+        
+        
+
+        
+       
+}
+
+function album() {
+ 
+    for (let i = 0; i < FiveRandomMusic.length; i++) {
+           
+        SearchResults.innerHTML += `<div><p>${FiveRandomMusic[i].album.title}</p></div>`  
+        
+        
+                     
+     }
+
+   
+}
+
+    FetchTotale("eminem")
+    FetchTotale("metallica")
+    FetchTotale("queen")
+    
+
