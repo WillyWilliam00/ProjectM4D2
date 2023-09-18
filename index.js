408fc3993f5a58a80c54955c49e245560e6db9e3
WillyWilliam00 <costawilliam98@gmail.com>
2023-09-18 21:34:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,16 +47,26 @@ function FetchTotale(artista) {
             const music = body.data
             const FiveRandomMusic = music.slice(0, 4)
             IdArtista.className = "pt-2"
-            for (let i = 0; i < FiveRandomMusic.length; i++) {
+            // FiveRandomMusic.forEach((music) => {
+            //     Row.innerHTML += `<div class="col-3 d-flex flex-column align-items-center">
+            //                          <h4>Album:</h4>
+            //                          <p style="font-size: 12px;">${music.album.title}</p>
+            //                          <img  src="${music.album.cover_medium}">
+            //                          <h6 class="pt-4 text-center">Titolo:</h6>
+            //                          <p class="text-center">${music.title}</p>
+            //                          </div>`
+            // });
+            
+             for (let i = 0; i < FiveRandomMusic.length; i++) {
 
-                Row.innerHTML += `<div class="col-3 d-flex flex-column align-items-center">
-                                    <h4>Album:</h4>
-                                    <p style="font-size: 12px;">${FiveRandomMusic[i].album.title}</p>
-                                    <img  src="${FiveRandomMusic[i].album.cover_medium}">
-                                    <h6 class="pt-4 text-center">Titolo:</h6>
-                                    <p class="text-center">${FiveRandomMusic[i].title}</p>
-                                    </div>`
-            }
+                 Row.innerHTML += `<div class="col-3 d-flex flex-column align-items-center">
+                                     <h4>Album:</h4>
+                                     <p style="font-size: 12px;">${FiveRandomMusic[i].album.title}</p>
+                                     <img  src="${FiveRandomMusic[i].album.cover_medium}">
+                                     <h6 class="pt-4 text-center">Titolo:</h6>
+                                     <p class="text-center">${FiveRandomMusic[i].title}</p>
+                                     </div>`
+             }
         })
         .catch(console.log("ERRORE"))
 }
