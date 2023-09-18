7517775d95be928d3bf4263a5698b0c7cd915615
WillyWilliam00 <133374335+WillyWilliam00@users.noreply.github.com>
2023-09-18 15:04:49 +0000

    ok
    ok

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const SearchArtista = document.querySelector("#searchField")
-const Eminem = document.querySelector("#eminem")
-const Metallica = document.querySelector("#metallica")
-const Queen = document.querySelector("#queen")
-function search() {
-    
+const EminemSection = document.querySelector("#eminemSection")
+const MetallicaSection = document.querySelector("#metallicaSection")
+const QueenSection = document.querySelector("#queenSection")
+const EminemID = document.querySelector("#eminem")
+const MetallicaID = document.querySelector("#metallica")
+const QueenID = document.querySelector("#queen")
 
-    clean()
-    if(SearchArtista.value){
+
+
+function clean() {
+    EminemSection.innerHTML = ""
+    MetallicaSection.innerHTML = ""
+    QueenSection.innerHTML = ""
+    EminemID.className = "d-none"
+    MetallicaID.className = "d-none"
+    QueenID.className = "d-none"
+}
+function search() {
+clean()
+    if (SearchArtista.value) {
         FetchTotale(SearchArtista.value)
     } else {
         alert("inserisci un'artista!")
-    } 
-}   
+    }
+}
+function ViewAll() {
+    clean()
+    FetchTotale("eminem")
+    FetchTotale("queen")
+    FetchTotale("metallica")
+}
+function trash() {
+    clean()
+}
 
 function FetchTotale(artista) {
     const IdArtista = document.querySelector(`#${artista}`)
     const Row = document.querySelector(`#${artista}Section`)
     const SearchResults = document.querySelector("#searchResults")
     SearchResults.removeAttribute('style');
+
+
     fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${artista}`)
         .then(response => response.json())
         .then(body => {
@@ -25,6 +48,7 @@ function FetchTotale(artista) {
             const FiveRandomMusic = music.slice(0, 4)
             IdArtista.className = "pt-2"
             for (let i = 0; i < FiveRandomMusic.length; i++) {
+
                 Row.innerHTML += `<div class="col-3 d-flex flex-column align-items-center">
                                     <h4>Album:</h4>
                                     <p style="font-size: 12px;">${FiveRandomMusic[i].album.title}</p>
@@ -32,14 +56,8 @@ function FetchTotale(artista) {
                                     <h6 class="pt-4 text-center">Titolo:</h6>
                                     <p class="text-center">${FiveRandomMusic[i].title}</p>
                                     </div>`
-            }   
+            }
         })
+        .catch(console.log("ERRORE"))
 }
 
-function clean() {
-
-    Eminem.className = "d-none"
-    Metallica.className = "d-none"
-    Queen.className = "d-none"
-    
-}
